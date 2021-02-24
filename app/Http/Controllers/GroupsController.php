a9e80245a3cf080318689e9cd3fbad36165406c6
Denis Ostrovsky <denisostrovskis1@gmail.com>
2021-02-25 00:37:16 +0200

    Added logic for fetch data from database

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class GroupsController extends Controller
 {
     public function index() {
-        return Inertia::render('Groups/Index');
+        $groupsTable = DB::table('groups')->get();
+        $topGroup = DB::table('groups')->select('group_code', 'group_name', 'score')->orderBy('score', 'desc')->first();
+
+        return Inertia::render('Groups/Index', ['groups' => $groupsTable, 'topGroup' => $topGroup]);
     }
 
     public function add() {
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index() {
-        return Inertia::render('Home/Index');
+        $groupsTable = DB::table('groups')->get();
+        $projectsTable = DB::table('projects')->get();
+        $studentsTable = DB::table('students')->get();
+
+        return Inertia::render('Home/Index', ['groups' => $groupsTable, 'projects' => $projectsTable, 'students' => $studentsTable]);
     }
 }
